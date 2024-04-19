292edc4e0a3b3772e3d454c14b9003038319ae31
AvdeepKaur <avkaur@bu.edu>
2024-04-19 18:19:14 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Typography } from "antd";
+import ResponsiveAppBar from "./components/appbar.js";
+import Box from "@mui/material/Box";
+import SwiperFunction from "./components/swiper.js";
+
+export default function Home() {
+  return (
+    <>
+      <SwiperFunction />
+    </>
+  );
+}
